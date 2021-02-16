a05aba32c697362bbb12db8a7ea244ff8ef7b837
Davis Keene <dbkeene.tsyc@gmail.com>
2021-02-16 13:54:19 -0600

    add: broken files, needs work

--- api/app.py
+++ api/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+import json
+from flask import Flask, request, jsonify
+from pymongo import Connection
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+c = Connection(host='db', port=27017)
+c.test_database
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='8000', debug=True)
