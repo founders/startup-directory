b1628a55e5fff24bb3c5c3c9f905ca5af2b722cd
vineetrc <vineetreddi@gmail.com>
2021-04-13 19:47:00 -0500

    working glyphicons

--- app/pages/profile/index.js
+++ app/pages/profile/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Form from '../../components/Form';
 import Layout from '../../components/Layout';
 import styles from '../../styles/Form.module.css';
+//import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+
 
 export default function Profile(){
     return (
       <Layout title="Edit your startup's profile">
       <p className={styles.desc}>
         Please Use this Form to add your startup to the Directory!
-      </p>
+      
+
+      <p className = {styles.center}  >
       <Form onSubmit={FormData => alert(JSON.stringify(FormData))} />
+      </p>
+      </p>
     </Layout>
     );
     
