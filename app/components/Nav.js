e8a837eb686cdaf61c84eb8938265fbab2e71efc
Aaron <aaronjalberg@gmail.com>
2021-04-18 18:37:12 -0500

    package fix

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -8,48 +8,6 @@ import { useUser } from '@auth0/nextjs-auth0';
  *
  * @returns {React.Component}
  */
-<<<<<<< HEAD
-const Nav = () => {
-  const { user, error, isLoading } = useUser();
-
-  return (
-    <nav className={styles.navWrapper}>
-      <Link href="/">LOGO</Link>
-      <ul className={styles.navLinks}>
-        <li>
-          <Link href="/" activeClassName="active-link" exact>
-            Directory
-          </Link>
-        </li>
-        <li>
-          <Link href="/about" activeClassName="active-link" exact>
-            About
-          </Link>
-        </li>
-        {!user && (
-          <li>
-            <Link href="/login" activeClassName="active-link" exact>
-              Login
-            </Link>
-          </li>
-        )}
-        {user && (
-          <li>
-            <Link href="/login" activeClassName="active-link" exact>
-              Logout
-            </Link>
-          </li>
-        )}
-        {user && (
-          <li>
-            <h2>{user.name}</h2>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
-};
-=======
 const Nav = () => (
   <nav className={styles.navWrapper}>
     <Link href="/">
@@ -76,6 +34,5 @@ const Nav = () => (
     </ul>
   </nav>
 );
->>>>>>> c3e50b56ecc77e78e08227b09bcb1b87166d413c
 
 export default Nav;
