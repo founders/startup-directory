963b8326ecbd28c749200aabac4c2c439bcf6044
Siraj <19193347+SirajChokshi@users.noreply.github.com>
2021-04-20 01:07:24 -0500

    add API wrapper

---
+++ app/utils/adminAPI.js
@@ -0,0 +1,20 @@
+export async function getWhitelist() {
+  const res = await fetch('/api/admin/whitelist/');
+  const json = await res.json();
+  return json;
+}
+
+export async function createWhitelist() {
+  const res = await fetch('/api/admin/whitelist/', { method: 'POST' });
+  const json = await res.json();
+  return json;
+}
+
+export async function updateWhitelist(whitelist) {
+  const res = await fetch('/api/admin/whitelist/', {
+    method: 'PATCH',
+    body: JSON.stringify({ data: whitelist }),
+  });
+  const json = await res.json();
+  return json;
+}
